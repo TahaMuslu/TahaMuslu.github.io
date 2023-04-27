1471361ac50a0c6e115cdf3c43d8007125584797
Taha Muslu <taha-_muslu1216@hotmail.com>
2023-04-27 19:57:40 +0300

    Site Bootstrap ile baştan yapıldı

--- meyve oyunu/js_oyun.js
+++ meyve oyunu/js_oyun.js
@@ -16,7 +16,7 @@ var createFruite = setInterval(fruitCreator, 500);
 function fruitCreator() {
     let Element = new Image();
     Element.classList.add("fruit");
-    Element.src = "/fruits/" + fruits[Math.floor(Math.random() * fruits.length)] + ".png";
+    Element.src = "./fruits/" + fruits[Math.floor(Math.random() * fruits.length)] + ".png";
     Element.style.left = Math.random() * (visualViewport.width - 150) + 50 + "px";
     Element.style.top = Math.random() * (visualViewport.height - 150) + 50 + "px";
     Element.addEventListener("click", function () {
